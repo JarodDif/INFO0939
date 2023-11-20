901cefbe52cba6940301894f7ff4477ee7107b95
JarodDif <jarod.differdange@gmail.com>
2023-11-20 09:29:26 +0100

    Reorganization of the example files

--- omp/example_inputs/audio3d/run_3d.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name="audio"
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=03:00
-#SBATCH --output=run_audio.out
-
-module load OpenMPI
-srun ../../fdtd param_audio.txt
