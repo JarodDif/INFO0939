d9f676593ac8bbf48f75d9b2992ace77e20b1557
S201900 <tom.bertrand@student.uliege.be>
2023-11-21 15:48:17 +0100

    Added 8x1 and 4x2 scripts

---
+++ example_inputs/simple3d/run_omp_1x8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="mpi_1x8"
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=00:04:00 # hh:mm:ss
+#SBATCH --output=run_mpi_1x8.out
+#SBATCH --mem-per-cpu=1024 # 1GB
+#SBATCH --partition=batch
+
+module load OpenMPI
+export OMP_NUM_THREADS=8
+
+srun  ../../omp/fdtd param_3d.txt
