233e2136bfffa1d2171ba7ac685742e89d64fe47
JarodDif <jarod.differdange@gmail.com>
2023-11-21 15:47:31 +0100

    Change job to use 64 processes

--- example_inputs/simple3d/run_mpi_big_r7.sh
+++ example_inputs/simple3d/run_mpi_big_r7.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name="mpi_run_custom"
-#SBATCH --ntasks=8
+#SBATCH --job-name="mpi_run_big_r7.sh"
+#SBATCH --ntasks=64
 #SBATCH --cpus-per-task=1
-#SBATCH --time=05:00
+#SBATCH --mem-per-cpu=4096
+#SBATCH --time=01:00:00
 #SBATCH --output=run_mpi_custom.out
 
 module load OpenMPI
