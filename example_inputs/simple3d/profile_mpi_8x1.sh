a9497cda192daf7c38fddd54d45a5ff35e6293a4
S201900 <tom.bertrand@student.uliege.be>
2023-11-26 23:05:23 +0100

    Added all profiles

--- example_inputs/simple3d/profile_mpi_8x1.sh
+++ example_inputs/simple3d/profile_mpi_8x1.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -l
 #
-#SBATCH --job-name="Scalasca profile"
+#SBATCH --job-name="profile_mpi_8x1"
 #SBATCH --ntasks=8
-#
+#SBATCH --cpus-per-task=1
 #SBATCH --time=00:04:00 # hh:mm:ss
-#SBATCH --output=run_mpi_8x1.out
+#SBATCH --output=profile_mpi_8x1.out
 #SBATCH --mem-per-cpu=1024 # 1GB
 #SBATCH --partition=batch
 
 # Loading Scalasca will also load OpenMPI
 module load Scalasca
+module load OpenMPI
 
 export OMP_PROC_BIND=true
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
 export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
 
-scalasca -analyze srun ./myapp_instr
+scalasca -analyze srun ../../mpi/fdtd param_3d.txt
