e9d72ffffbbd1ec94a55593763dd4015abfbeada
S201900 <tom.bertrand@student.uliege.be>
2023-11-28 14:58:01 +0100

    Removing summaries, adding trace jobs

---
+++ example_inputs/simple3d/trace_mpi_8x1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name="trace_mpi_8x1"
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:04:00 # hh:mm:ss
+#SBATCH --output=trace_mpi_8x1.out
+#SBATCH --mem-per-cpu=1024 # 1GB
+#SBATCH --partition=batch
+
+# Loading Scalasca will also load OpenMPI
+module load Scalasca
+module load OpenMPI
+
+export OMP_PROC_BIND=true
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+
+scalasca -analyze -q -t -f scorep.filter srun ../../mpi/fdtd param_3d.txt
---
+++ example_inputs/simple3d/trace_mpi_omp_4x2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#
+#SBATCH --job-name="profile_mpi_omp_4x2"
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=2
+#SBATCH --time=00:04:00 # hh:mm:ss
+#SBATCH --output=profile_mpi_omp_4x2.out
+#SBATCH --mem-per-cpu=1024 # 1GB
+#SBATCH --partition=batch
+
+module load Scalasca
+module load OpenMPI
+
+export OMP_PROC_BIND=true
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+
+scalasca -analyze -q -t -f scorep.filter srun ../../mpi_omp/fdtd param_3d.txt
