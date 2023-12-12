0d89d67b9c8942c03277ab73f1edfd1782f6a729
S201900 <tom.bertrand@student.uliege.be>
2023-12-12 14:53:17 +0100

    Added Young slits

---
+++ young/young.py
@@ -0,0 +1,44 @@
+import numpy as np
+import struct
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+dims = (101, 2, 101)
+lims = (0,1), (0,1), (0,1)
+
+def write_int(f, n: int):
+    f.write(n.to_bytes(4, byteorder="little"))
+
+def write_double(f, d:float):
+    f.write(struct.pack('d', d))
+
+def write_dims(f, dims:tuple[int,int,int]):
+    for d in dims:
+        write_int(f, d)
+
+def write_lims(f, lims:tuple[tuple[float, float], tuple[float, float], tuple[float, float]]):
+    for (l,u) in lims:
+        write_double(f, l)
+        write_double(f, u)
+
+# Will write for stepindex 0, timestep 0
+def write_data(f, vals):
+    write_int(f, 0)
+    write_double(f, 0)
+    for p in range(vals.shape[2]):
+            for n in range(vals.shape[1]):
+                for m in range(vals.shape[0]):
+                    write_double(f, vals[m,n,p])
+
+vals = np.full(dims, 340)
+
+for i in range(len(vals)):
+    for j in range(len(vals[i])):
+        vals[i, j, dims[2]//2] = 0
+        if i == 45 or i == 55:
+            vals[i, :, dims[2]//2] = 340
+
+with open("../example_inputs/simple3d/in_c_custom_young.dat", "wb") as f:
+    write_dims(f,dims)
+    write_lims(f,lims)
+    write_data(f,vals)
