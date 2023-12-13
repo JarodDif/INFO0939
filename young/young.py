b0d9177fd550347132869dfc9bca493df8692dbf
S201900 <tom.bertrand@student.uliege.be>
2023-12-13 21:53:09 +0100

    Added visualization of Young slits

--- young/young.py
+++ young/young.py
@@ -1,5 +1,6 @@
 import numpy as np
 import struct
+import matplotlib.pyplot as plt
 
 dims = (101, 2, 101)
 lims = (0,1), (0,1), (0,1)
@@ -24,17 +25,26 @@ def write_data(f, vals):
     write_int(f, 0)
     write_double(f, 0)
     for p in range(vals.shape[2]):
-            for n in range(vals.shape[1]):
-                for m in range(vals.shape[0]):
-                    write_double(f, vals[m,n,p])
+        for n in range(vals.shape[1]):
+            for m in range(vals.shape[0]):
+                write_double(f, vals[m,n,p])
 
-vals = np.full(dims, 340)
+vals                    = np.full(dims, 340)
+vals[:, :, dims[2]//2]  = 0
+vals[45, :, dims[2]//2] = 340
+vals[55, :, dims[2]//2] = 340
 
-for i in range(len(vals)):
-    for j in range(len(vals[i])):
-        vals[i, j, dims[2]//2] = 0
-        if i == 45 or i == 55:
-            vals[i, :, dims[2]//2] = 340
+im = plt.imshow(vals[:, :, dims[2]//2], aspect='auto')
+plt.colorbar(im)
+plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+indices = np.where(vals == 0)
+print(indices)
+ax.scatter(*indices, c="k")
+plt.show()
 
 with open("../example_inputs/simple3d/in_c_custom_young.dat", "wb") as f:
     write_dims(f,dims)
