f54fb018371783c274f58afd3bd946fff16e1bb9
JarodDif <jarod.differdange@gmail.com>
2023-12-17 11:16:28 +0100

    Added run script

---
+++ example_inputs/simple3d/run_mgpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="mpgu_point"
+#SBATCH --output="run_mgpu.out"
+#SBATCH --partition=gpu
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=15:00
+#SBATCH --account=ulghpsc
+
+module load OpenMPI/4.1.5-Clang-16.0.6-GFortran-11.3.0-CUDA-11.7.0
+
+export OMP_TARGET_OFFLOAD=MANDATORY
+
+srun ../../mpi_gpu_subdomain_save/fdtd param_3d.txt
