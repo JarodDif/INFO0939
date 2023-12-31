6a202582165441a71dd50e4038f15b0e438f72b1
JarodDif <jarod.differdange@gmail.com>
2023-12-31 21:05:29 +0100

    new data

--- mpi/read_data.py
+++ mpi/read_data.py
@@ -6,12 +6,12 @@ from scipy.optimize import curve_fit
 data_ss_111 = pd.read_csv('./strong_scaling/data_111.csv')
 data_ss_222 = pd.read_csv('./strong_scaling/data_222.csv')
 data_ws_1e6 = pd.read_csv('./weak_scaling/data_1e6.csv')
-data_ws_4e6 = pd.read_csv('./weak_scaling/data_4e6.csv')
+data_ws_8e6 = pd.read_csv('./weak_scaling/data_8e6.csv')
 
 ref_111     = data_ss_111["time"][0]
 ref_222     = data_ss_222["time"][0]
 ref_1e6     = data_ws_1e6["time"][0]
-ref_4e6     = data_ws_4e6["time"][0]
+ref_8e6     = data_ws_8e6["time"][0]
 
 amdahl      = lambda np, p: 1/((1-p) + p/np )
 gustafson   = lambda np, p: (1-p) + p * np
@@ -20,7 +20,7 @@ popt_ss_111 = curve_fit(amdahl, data_ss_111["processes"], ref_111/data_ss_111["t
 popt_ss_222 = curve_fit(amdahl, data_ss_222["processes"], ref_222/data_ss_222["time"])[0][0]
 
 popt_ws_1e6 = curve_fit(gustafson, data_ws_1e6["processes"], ref_1e6/data_ws_1e6["time"]*data_ws_1e6["processes"])[0][0]
-popt_ws_4e6 = curve_fit(gustafson, data_ws_4e6["processes"], ref_4e6/data_ws_4e6["time"]*data_ws_4e6["processes"])[0][0]
+popt_ws_8e6 = curve_fit(gustafson, data_ws_8e6["processes"], ref_8e6/data_ws_8e6["time"]*data_ws_8e6["processes"])[0][0]
 
 np_tab      = np.linspace(1, 64, 100)
 
@@ -37,8 +37,8 @@ plt.show()
 
 plt.plot(data_ws_1e6["processes"], ref_1e6/data_ws_1e6["time"]*data_ws_1e6["processes"], "-ob", label="$10^6$ elements per process")
 plt.plot(np_tab, gustafson(np_tab, popt_ws_1e6), "--b", label=f"Gustafson's law with $p ={popt_ws_1e6:.2f}$")
-plt.plot(data_ws_4e6["processes"], ref_4e6/data_ws_4e6["time"]*data_ws_4e6["processes"], "-og", label="$4 \\ 10^6$ elements per process")
-plt.plot(np_tab, gustafson(np_tab, popt_ws_4e6), "--g", label=f"Gustafson's law with $p ={popt_ws_4e6:.2f}$")
+plt.plot(data_ws_8e6["processes"], ref_8e6/data_ws_8e6["time"]*data_ws_8e6["processes"], "-og", label="$8 \\ 10^6$ elements per process")
+plt.plot(np_tab, gustafson(np_tab, popt_ws_8e6), "--g", label=f"Gustafson's law with $p ={popt_ws_8e6:.2f}$")
 plt.xlabel('Number of processes')
 plt.ylabel('Scaled Speedup')
 plt.grid()
