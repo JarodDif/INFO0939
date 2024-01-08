8a181e7cdce5c3d4c76a093c10cc15888a52773e
S201900 <tom.bertrand@student.uliege.be>
2024-01-08 13:14:09 +0100

    added strong scaling hybrid

--- mpi_omp/strong_scaling/run_222_16.sh
+++ mpi_omp/strong_scaling/run_222_16.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name="ss_22216"
 #SBATCH --output="run_22216.out"
-#SBATCH --ntasks=16
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4
 #SBATCH --time=00:20:00 # hh:mm:ss
 #SBATCH --mem-per-cpu=10240
 #SBATCH --partition=hmem
 #SBATCH --exclusive
 
 module load OpenMPI
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
 
 srun ../fdtd param_3d_200_200_200.txt
