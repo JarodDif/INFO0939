c916039f917535d8c339376b4aeff9db6300fa8a
JarodDif <jarod.differdange@gmail.com>
2024-01-08 14:27:56 +0100

    Create omp r7 script

---
+++ example_inputs/r7/run_omp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="omp_r7"
+#SBATCH --output="run_omp.out"
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=01:00:00
+
+module load OpenMPI
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+
+srun ../../omp/fdtd param_r7.txt
